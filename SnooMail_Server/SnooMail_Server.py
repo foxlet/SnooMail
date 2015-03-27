ce3ee8653222eb4f5971f99c1a7f72af87d75535
Foxlet <inquiries@comprepair.tk>
2015-03-26 21:28:08 -0400

    Brimir Support - Fixed

--- SnooMail_Server/SnooMail_Server.py
+++ SnooMail_Server/SnooMail_Server.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 
 id_from = "Example <support@example.com>"
 remote_mailgun = "https://api.mailgun.net/v2/example.com/messages"
+remote_brimir = "https://brimir.example.com/{}"
+
+def get_token(user, password):
+    auth = requests.post(remote_brimir.format('api/v1/sessions'), data={'email': user,'password': password}).json()
+    return auth['authorization_token']
 
 @app.route('/send', methods=['POST'])
 def send_msg():
@@ -24,7 +29,30 @@ def send_msg():
 
 @app.route('/receive', methods=['POST'])
 def recv_msg():
-    id_no = 'test'
+    msg_route = {}
+    msg_route['recipient'] = request.form['recipient']
+    msg_route['sender'] = request.form['sender']
+    msg_route['body'] = request.form['body-plain']
+    msg_route['from'] = request.form['From']
+    msg_route['to'] = request.form['To']
+    msg_route['subject'] = request.form['Subject']
+
+    # May be needed in the future? Currently not used
+    # token = get_token('', '')
+    # request_url = 'tickets.json?auth_token={}'.format(token)
+    request_url = 'tickets.json'
+
+    email = 'To: {}\n'\
+            'Subject: {}\n'\
+            'From: {}\n'\
+            '\n{}'.format(msg_route['to'], msg_route['subject'], msg_route['from'], msg_route['body'])
+
+    print(email)
+
+    payload = requests.post(remote_brimir.format(request_url),
+                data={'message': email})
+
+    return payload.text
 
 if __name__ == '__main__':
     app.run()
